2f30d4750f86d7ca26db6aa1f899a22e571cb5bd
Jeremy Qian <jq357@hotmail.com>
2022-08-29 20:59:52 -0400

    setup

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { ThemeProvider, Button, createTheme } from '@rneui/themed';
+import { NavigationContainer } from '@react-navigation/native';
+import { LogIn } from './src/pages/login';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const Stack = createNativeStackNavigator();
+
+const theme = createTheme({
+  components: {
+    Button: {
+      raised: true,
+    },
   },
 });
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={LogIn}
+            options={{ title: 'Login' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
+  );
+};
+
+export default App;
