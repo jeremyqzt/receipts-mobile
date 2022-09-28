3590556ecf3e4bdd22814dd836fa0c68b562900f
Jeremy Qian <jq357@hotmail.com>
2022-09-27 20:21:23 -0400

    set active bucket

--- src/pages/screens/settings.js
+++ src/pages/screens/settings.js
@@ -1,8 +1,27 @@
 import { Input, Button } from "@rneui/themed";
 import * as SecureStore from "expo-secure-store";
 import { View, Image, Text, Linking } from "react-native";
+import { listBuckets, getActiveBucket } from "../../utils/bucketUtils";
+import { useEffect, useState } from "react";
 
 export const SettingsScreen = () => {
+
+  const [buckets, setBuckets] = useState([]);
+  const [activeBucket, setActiveBucket] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    listBuckets().then((res)=>{
+      //console.log(res);
+      setBuckets(res);
+    });
+
+    getActiveBucket().then((res)=>{
+      console.log(res);
+      setActiveBucket(res);
+    });
+
+  }, [setActiveBucket, setBuckets, setLoading]);    
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Button 
