4e4b464e94f57ff69ce79a93c568bca48d7103a4
Jeremy Qian <jq357@hotmail.com>
2022-10-01 15:57:24 -0400

    icons

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,25 +1,40 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SettingsScreen } from "./screens/settings";
 import { HomeScreen } from "./screens/home";
+import { Icon } from "@rneui/themed";
 
 const Tab = createBottomTabNavigator();
-
 export const Home = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen
-        options={{ headerShown: false }}
         name="Home"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <Icon name="receipt" type="font-awesome-5" color={color} />
+          ),
+        }}
         component={HomeScreen}
       />
       <Tab.Screen
-        options={{ headerShown: false }}
-        name="Settings"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <Icon name="upload" type="font-awesome-5" color={color} />
+          ),
+        }}
+        name="Upload"
         component={SettingsScreen}
       />
       <Tab.Screen
-        options={{ headerShown: false }}
-        name="Upload"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <Icon name="cog" type="font-awesome-5" color={color} />
+          ),
+        }}
+        name="Settings"
         component={SettingsScreen}
       />
     </Tab.Navigator>
