a710ac16795c769a0243ff4decf4c42bf60de017
Jeremy Qian <jq357@hotmail.com>
2022-10-04 21:30:33 -0400

    load camera

--- src/pages/screens/upload.js
+++ src/pages/screens/upload.js
@@ -1,16 +1,27 @@
 import { Input, Button } from "@rneui/themed";
 import * as SecureStore from "expo-secure-store";
-import { View, Image, Text, Linking } from "react-native";
+import { View, Image, Text, Linking, StyleSheet, TouchableOpacity } from "react-native";
 import { listBuckets, getActiveBucket } from "../../utils/bucketUtils";
 import { useEffect, useState } from "react";
 import { BucketsSelect } from "../../components/settings/buckets";
 import Toast from "react-native-toast-message";
 import * as ImagePicker from "expo-image-picker";
+import { Camera, CameraType } from 'expo-camera';
 
 export const UploadScreen = () => {
   const [buckets, setBuckets] = useState([]);
   const [activeBucket, setActiveBucket] = useState({});
   const [loading, setLoading] = useState(false);
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  useEffect(() => {
+    requestPermission();
+  });
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
 
   const [image, setImage] = useState(null);
 
@@ -30,7 +41,9 @@ export const UploadScreen = () => {
     }
   };
 
+
   useEffect(() => {
+    
     listBuckets().then((res) => {
       setBuckets(res);
     });
@@ -39,6 +52,14 @@ export const UploadScreen = () => {
       setActiveBucket(res);
     });
   }, [setActiveBucket, setBuckets, setLoading]);
+
+  if (!permission) {
+    return null
+  } 
+
+  if (!permission.granted) {
+    return null
+  } 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Toast />
@@ -53,6 +74,23 @@ export const UploadScreen = () => {
       >
         <BucketsSelect buckets={buckets} activeBucket={activeBucket} />
       </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      </View>
+
       <Button
         title={"Upload!"}
         onPress={async () => {
@@ -62,3 +100,75 @@ export const UploadScreen = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    inputIcon: {
+      marginTop: 5,
+      fontSize: 35,
+    },
+    header: {
+      marginHorizontal: 15,
+      marginBottom: 20,
+    },
+    inputContainer: {
+      display: "flex",
+      flexDirection: "row",
+      marginHorizontal: 10,
+      marginVertical: 5,
+    },
+    buttomBar: {
+      flex: 1,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    bottomBtn: {
+      flex: 1,
+    },
+  
+    dialog: {
+      backgroundColor: "white",
+      padding: 15,
+    },
+    container: {
+      display: "flex",
+      flexDirection: "row",
+      backgroundColor: "white",
+      padding: 10,
+    },
+    dropdown: {
+      height: 45,
+      width: "85%",
+      borderColor: "gray",
+      borderWidth: 0.5,
+      borderRadius: 0,
+      paddingLeft: 10,
+    },
+    icon: {
+      marginRight: 5,
+    },
+    label: {
+      position: "absolute",
+      backgroundColor: "white",
+      left: 22,
+      top: 8,
+      zIndex: 999,
+      paddingHorizontal: 8,
+      fontSize: 14,
+    },
+    placeholderStyle: {
+      fontSize: 16,
+    },
+    selectedTextStyle: {
+      fontSize: 16,
+    },
+    iconStyle: {
+      width: 20,
+      height: 20,
+    },
+    inputSearchStyle: {
+      height: 40,
+      fontSize: 16,
+    },
+  });
+  
