db619ada054fd39b0fcbfdc4aff4b3996ec14483
Jeremy Qian <jq357@hotmail.com>
2022-10-12 21:43:09 -0400

    spacing

--- src/components/mainTable/mainTable.js
+++ src/components/mainTable/mainTable.js
@@ -46,46 +46,64 @@ export const MainTable = (props) => {
         renderItem={({ item, index }) => {
           return (
             <ListItem bottomDivider>
-              <TouchableOpacity
-                onPress={() => {
-                  setActiveReceipt(item);
-                  setModalOpen(true);
+              <View
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
                 }}
               >
-                <Avatar
-                  title={item.alive}
-                  source={item.image_url && { uri: item.image_url }}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => {
-                  setActiveReceiptIdx(index);
-                  setActiveReceipt(item);
-                  setEditModalOpen(true);
-                }}
-              >
-                <ListItem.Content>
-                  <ListItem.Title>
-                    <Text
-                      style={{ fontWeight: "bold" }}
-                    >{`${item.vendor} `}</Text>
-                  </ListItem.Title>
-                  <ListItem.Subtitle>{`Receipt Total: $${item.total_amount} `}</ListItem.Subtitle>
-                  <ListItem.Subtitle>{`Purchase Date: ${item.receipt_date} `}</ListItem.Subtitle>
-                  <ListItem.Subtitle>{`Purchase Category: ${
-                    categories[item.category - 1]?.name
-                  } `}</ListItem.Subtitle>
-                </ListItem.Content>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => {
-                  setActiveReceiptIdx(idx);
-                  setActiveReceipt(item);
-                  setEditModalOpen(true);
-                }}
-              >
-                <ListItem.Chevron />
-              </TouchableOpacity>
+                <View
+                  style={{
+                    width: "75%",
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "flex-start",
+                  }}
+                >
+                  <TouchableOpacity
+                    onPress={() => {
+                      setActiveReceipt(item);
+                      setModalOpen(true);
+                    }}
+                  >
+                    <Avatar
+                      title={item.alive}
+                      source={item.image_url && { uri: item.image_url }}
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => {
+                      setActiveReceiptIdx(index);
+                      setActiveReceipt(item);
+                      setEditModalOpen(true);
+                    }}
+                  >
+                    <ListItem.Content>
+                      <ListItem.Title>
+                        <Text
+                          style={{ fontWeight: "bold" }}
+                        >{`${item.vendor} `}</Text>
+                      </ListItem.Title>
+                      <ListItem.Subtitle>{`Receipt Total: $${item.total_amount} `}</ListItem.Subtitle>
+                      <ListItem.Subtitle>{`Purchase Date: ${item.receipt_date} `}</ListItem.Subtitle>
+                      <ListItem.Subtitle>{`Purchase Category: ${
+                        categories[item.category - 1]?.name
+                      } `}</ListItem.Subtitle>
+                    </ListItem.Content>
+                  </TouchableOpacity>
+                </View>
+                <TouchableOpacity
+                  onPress={() => {
+                    setActiveReceiptIdx(index);
+                    setActiveReceipt(item);
+                    setEditModalOpen(true);
+                  }}
+                >
+                  <ListItem.Chevron />
+                </TouchableOpacity>
+              </View>
             </ListItem>
           );
         }}
