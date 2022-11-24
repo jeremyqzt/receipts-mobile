082f997e0bbd5240d9d624a7b262665713f2e2bd
Jeremy Qian <jq357@hotmail.com>
2022-11-23 22:00:31 -0500

    select on categories

--- src/components/charts/categoryTable.js
+++ src/components/charts/categoryTable.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { getTotalMonthlyCosts } from "../../utils/chartUtils";
 import { useFetch } from "../../hooks/index";
 import { monthName } from "../../constants/chartConstants";
@@ -14,9 +14,12 @@ import {
   Cols,
   Cell,
 } from "react-native-table-component";
+import MultiSelect from "react-native-multiple-select";
 
 export const CategoryTables = () => {
   const { response: chartResp } = useFetch(getTotalMonthlyCosts);
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
   const transformedCategories = {};
 
@@ -38,10 +41,11 @@ export const CategoryTables = () => {
   }
 
   const usableCategories = categories.map((c) => ({
-    key: c.value,
-    label: c.name,
+    id: c.value,
+    name: c.name,
   }));
-  const field = [];
+
+  const field = selectedItems;
   const data = [
     [
       "Categories",
@@ -59,11 +63,8 @@ export const CategoryTables = () => {
       ];
     }),
   ];
-  console.log(data);
 
-  const width = data[0].map(() => 100)
-
-  // return null;
+  const width = data[0].map(() => 120);
 
   return (
     <>
@@ -78,19 +79,75 @@ export const CategoryTables = () => {
       >
         Category Totals
       </Text>
-      <View style={{ height: 800, marginTop: 20 }}>
-        <ScrollView style={{height: 800}} horizontal={true} alwaysBounceHorizontal={false} bounces={false}>
-          <Table borderStyle={{ borderWidth: 1, borderColor: "#C1C0B9" }}>
-            {data.map((rowData, index) => (
-              <Row
-                key={index}
-                data={rowData}
-                style={[index % 2 && { backgroundColor: "#F7F6E7" }, {height: 25}]}
-                widthArr={width}
-                textStyle={[{ fontSize: 12 }]}
-              />
-            ))}
-          </Table>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-end",
+          marginTop: 20,
+        }}
+      >
+        <View style={{ display: "flex", width: "70%", marginHorizontal: "1%" }}>
+          <MultiSelect
+            hideTags
+            items={usableCategories}
+            uniqueKey="id"
+            onSelectedItemsChange={(s) => {
+              setSelectedItems(s);
+            }}
+            onToggleList={() => {
+              setIsOpen((a) => !a);
+            }}
+            onClearSelector={() => {
+              setIsOpen(false);
+            }}
+            styleSelectorContainer={{height: Dimensions.get("window").height * 0.5}}
+            styleRowList={{marginVertical: 3}}
+            fontSize={18}
+            itemFontSize={18}
+            selectedItems={selectedItems}
+            selectText="Pick Categories"
+            tagRemoveIconColor="#CCC"
+            tagBorderColor="#CCC"
+            tagTextColor="#CCC"
+            selectedItemTextColor="#CCC"
+            selectedItemIconColor="#CCC"
+            itemTextColor="#000"
+            displayKey="name"
+            searchInputStyle={{ color: "#CCC" }}
+            textInputProps={{ editable: false, autoFocus: false }}
+            searchInputPlaceholderText=""
+            searchIcon={false}
+            hideSubmitButton
+          />
+        </View>
+      </View>
+      <View style={{ height: 1000, marginTop: 12 }}>
+        <ScrollView
+          style={{ height: 700 }}
+          horizontal={true}
+          alwaysBounceHorizontal={false}
+          bounces={false}
+        >
+          {!isOpen ? (
+            <Table borderStyle={{ borderWidth: 1, borderColor: "#C1C0B9" }}>
+              {data.map((rowData, index) => (
+                <Row
+                  key={index}
+                  data={rowData}
+                  style={[
+                    index % 2 && { backgroundColor: "#F5F5F5" },
+                    { height: 25 },
+                  ]}
+                  widthArr={width}
+                  textStyle={[
+                    { fontSize: 16, textAlign: "center" },
+                    index === 0 && { fontWeight: "bold" },
+                  ]}
+                />
+              ))}
+            </Table>
+          ) : null}
         </ScrollView>
       </View>
     </>
